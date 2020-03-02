60e2bebbdc004837d95f066dfde74a2862e41936
Carly Langlois <cj.langlois.cl@gmail.com>
2020-03-02 09:57:53 -0600

    end of lesson content walkthrough

---
+++ Car/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CarNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Toyota", "Prius", 10, 50);
+            Console.WriteLine(car.Make + " - " + car.Model);
+        }
+    }
+}
---
+++ CarTests/CarTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarNS;
+
+namespace CarTests
+{
+    [TestClass]
+    public class CarTests
+    {
+        //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(10, 10, .001);
+        }
+
+        Car test_car;
+
+        [TestInitialize]
+        public void CreateCarObject()
+        {
+            test_car = new Car("Toyota", "Prius", 10, 50);
+        }
+
+        //TODO: constructor sets gasTankLevel properly
+        [TestMethod]
+        public void TestInitialGasTank()
+        {
+            Assert.AreEqual(10, test_car.GasTankLevel, .001);
+        }
+
+        //TODO: gasTankLevel is accurate after driving within tank range
+        //TODO: gasTankLevel is accurate after attempting to drive past tank range
+        //TODO: can't have more gas than tank size, expect an exception
+
+    }
+}
